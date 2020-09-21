e3c0e6e9571419ce87349139845f5eeef2869388
Kerem <52282189+KeremOzfo@users.noreply.github.com>
2020-09-21 12:16:22 +0300

    Add files via upload

--- train_funcs.py
+++ train_funcs.py
@@ -68,7 +68,7 @@ def train_vanilla(args, device):
     trun = 0
     topk = math.ceil(modelsize / args.majority_Sparsity)
     majority_pool = torch.zeros(modelsize).to(device)
-    majority_mask = torch.ones(modelsize).to(device)
+    majority_mask = torch.zeros(modelsize).to(device)
 
     for epoch in tqdm(range(args.num_epoch)):
         atWarmup = (args.warmUp and epoch < 5)
@@ -85,7 +85,7 @@ def train_vanilla(args, device):
 
                 trainloader = DataLoader(dl.DatasetSplit(trainset, sample_inds[cl]), batch_size=args.bs,
                                          shuffle=True)
-                for data in trainloader:
+                for data in trainloader: ## Train
                     inputs, labels = data
                     inputs, labels = inputs.to(device), labels.to(device)
                     optimizers[cl].zero_grad()
@@ -94,50 +94,54 @@ def train_vanilla(args, device):
                     loss.backward()
                     optimizers[cl].step()
                     break
-            trun += 1
-            atCommRound = ((args.LocalSGD and trun == args.LSGDturn) or not args.LocalSGD)
-            if atWarmup:
+            trun += 1 ## Local Iter Counter
+            atCommRound = ((args.LocalSGD and trun == args.LSGDturn) or not args.LocalSGD) ## check whether this is the communication round
+            if atWarmup: ## warm up phase
                 ps_model_flat = sf.get_model_flattened(net_ps, device)
                 ps_model_dif = torch.zeros_like(ps_model_flat).to(device)
-                for cl in range(num_client):
+                for cl in range(num_client): ## get average of all workers' model difference
                     model_flat = sf.get_model_flattened(net_users[cl], device)
                     ps_model_dif.add_(1 / num_client, model_flat.sub(1,ps_model_flat))
-                ps_model_flat.add_(1, ps_model_dif)
+                ps_model_flat.add_(1, ps_model_dif) ## update ps_model
                 sf.make_model_unflattened(net_ps, ps_model_flat, net_sizes, ind_pairs)
-                [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)]
+                [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)] ## update worker models
                 trun = 0
-            elif not atWarmup and atCommRound:
-                majority_pool *= 0
-                clone_mask = torch.clone(majority_mask).to(device)
+            elif not atWarmup and atCommRound: ## communication Round
+                majority_pool *= 0 ## reset majority pool
+                clone_mask = torch.clone(majority_mask).to(device) ## reserve the previous iter mask
                 ps_model_flat = sf.get_model_flattened(net_ps, device)
                 ps_model_dif = torch.zeros_like(ps_model_flat).to(device)
                 difmodels = []
-                wtow_corrs = []
-                wto_prevmask_corrs = []
+                wtow_corrs = [] ## stats for wlocal_wlocal
+                wto_prevmask_corrs = [] ## stats for wlocal_to_prevMask
                 for cl in range(num_client):
                     model_flat = sf.get_model_flattened(net_users[cl], device)
-                    if not args.LocalSGD:
+                    if not args.LocalSGD: ## error correction
                         if epoch in args.lr_change and run == 0:
                             model_flat.add_(errors[cl].mul_(0.1))
                         else:
                             model_flat.add_(errors[cl])
-                    else:
+                    else: ## Error Correction if LocalSGD is on
                         if epoch in args.lr_change and run < trun:
                             model_flat.add_(errors[cl].mul_(0.1))
                         else:
                             model_flat.add_(errors[cl])
-                    difmodel = (model_flat.sub(1,ps_model_flat)).to(device)
-                    worker_mask = torch.zeros(modelsize).to(device)
+                    difmodel = (model_flat.sub(1,ps_model_flat)).to(device) ### get new difference of the local worker after after errorCor
 
-                    worker_mask[torch.topk(difmodel.abs(),k=topk,dim=0)[1]] = 1
+                    ###Stats##
+                    worker_mask = torch.zeros(modelsize).to(device) ## worker_mask for stat
+                    worker_mask[torch.topk(difmodel.abs(),k=topk,dim=0)[1]] = 1 ## make workermask
                     wto_prevmask_corrs.append(torch.sum(majority_mask.mul(worker_mask)) * 100 /topk)
                     wtow_corrs.append(torch.sum(worker_masks[cl].mul(worker_mask)) * 100 / topk)
-
                     worker_masks[cl] = worker_mask
-                    difmodels.append(difmodel)
-                    majority_pool[torch.topk(difmodel.abs(), k=topk, dim=0)[1]] += 1
-                majority_mask.mul_(0)
-                majority_mask[torch.topk(majority_pool, k=topk, dim=0)[1]] = 1
+                    ##Stats###
+
+                    difmodels.append(difmodel) ## store worker's model Differences
+                    majority_pool[torch.topk(difmodel.abs(), k=topk, dim=0)[1]] += 1 ## Collect all topk values into a pool
+                majority_mask.mul_(0) ## Reset the mask
+                majority_mask[torch.topk(majority_pool, k=topk, dim=0)[1]] = 1 ## make new mask from the pool
+
+                ###STAT outputs###
                 print('w_to_prevm')
                 print(wto_prevmask_corrs)
                 print('w_to_w')
@@ -145,13 +149,15 @@ def train_vanilla(args, device):
                 print('mask_to_mask')
                 corr2 = torch.sum(majority_mask.mul(clone_mask))
                 print(corr2 * 100 /topk)
+                #####
+
                 for cl in range(num_client):
-                    ps_model_dif.add_(1 / num_client, difmodels[cl].mul(majority_mask))
-                    errors[cl] = difmodels[cl].mul(1 - majority_mask)
-                ps_model_flat.add_(1, ps_model_dif)
+                    ps_model_dif.add_(1 / num_client, difmodels[cl].mul(majority_mask)) ## sparse and add the worker diff Values
+                    errors[cl] = difmodels[cl].mul(1 - majority_mask) ## update Errors
+                ps_model_flat.add_(1, ps_model_dif) ## update model of the ps
                 sf.make_model_unflattened(net_ps, ps_model_flat, net_sizes, ind_pairs)
-                [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)]
-                trun = 0
+                [sf.pull_model(net_users[cl], net_ps) for cl in range(num_client)] ### update local woker model's
+                trun = 0 ## reset localTurn
 
             # if run % 25 == 0:  ##debug
             #     acc = evaluate_accuracy(net_ps, testloader, device)
